c8fa5e5cffc81ed0a92768d5515f7d4ebbf28456
Mayembe <zephinium@gmail.com>
2023-01-12 16:37:00 +0100

    Fin de la section background Image debut de la section qui sommes nous

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import Header from "../components/Header";
 import "../styles/globals.css";
 import "../styles/Footer.css";
+import "../styles/Home.css";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
