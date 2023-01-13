ff0957a544d9cf3fe7bca203e43a0c01fc63af44
Mayembe <zephinium@gmail.com>
2023-01-13 11:44:47 +0100

    Réinitialisation du projet

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import { Fragment } from "react";
-import Header from "../components/Header";
+
 import "../styles/globals.css";
-import "../styles/Footer.css";
-import "../styles/Home.css";
-import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
-      <Header />
       <Component {...pageProps} />
-      <Footer />
     </Fragment>
   );
 }
