b04b98cdb8ab245430a7534b2ac58882cdbe92fd
flyts <samytshibanda@gmail.com>
2023-01-13 12:59:10 +0100

    Refactor: Structuration du projet

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/footer.module.css"
+
+function Footer()
+{
+    const component = 
+    <>
+        <h1>Footer</h1>
+    </>
+
+    return component
+}
+
+export default Footer
---
+++ components/NavBar.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/navbar.module.css"
+
+function NavBar()
+{
+    const component = 
+    <>
+        <h1>Nav</h1>
+    </>
+
+    return component
+}
+
+export default NavBar
