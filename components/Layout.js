e5c1b19efd2d556329808977290346e3fa69911c
Mayembe <zephinium@mail.com>
2023-05-20 11:57:36 +0200

    refactor color and text

--- components/Layout.js
+++ components/Layout.js
@@ -73,13 +73,11 @@ function Layout({ children }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <NavBar />
 
         <main>
           {children}
         </main>
-
         <Footer />
       </div>
     </dataContext.Provider>
